2f18b248aacffda26b2add16b7f48214a658b6b5
OceanChe <darikcheee@gmail.com>
2021-06-11 17:58:55 +0300

    Homework 5 styles

--- che_app/src/components/Task.js
+++ che_app/src/components/Task.js
@@ -1,12 +1,19 @@
 import React from "react";
+import classnames from "classnames/bind";
+import styles from "../styles/styles.module.scss"
+import { ThemeContext } from "../ThemeContext";
+
+const cx = classnames.bind(styles)
 
 export class Task extends React.Component {
+    static contextType = ThemeContext
+
     render = () => {
       return (
-        <div key={this.props.id} class="Tasks">
-          <div class="ID"> {this.props.id}</div> 
-          <div class="name"><h1>{this.props.name}</h1></div> 
-          <div class="description">{this.props.description}</div>
+        <div key={this.props.id} className={cx("Tasks", `Tasks-theme-${this.context}`)}>
+          <div className={cx("ID", `ID-theme-${this.context}`)}> {this.props.id}</div> 
+          <div className={cx("name", `name-theme-${this.context}`)}><h1 className={cx(`theme-${this.context}`)}>{this.props.name}</h1></div> 
+          <div className={cx("description", `description-theme-${this.context}`)}>{this.props.description}</div>
           <button onClick={this.onStatusClick}>{this.props.completed.toString()}</button>
         </div>
     )}
